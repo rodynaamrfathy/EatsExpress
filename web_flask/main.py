3987af475bb00827fab1950b280c8d227c16d7a2
rodynaamrfathy <rodynaamr@icloud.com>
2024-06-23 16:35:27 +0300

    ‘V2’

--- web_flask/main.py
+++ web_flask/main.py
@@ -3,10 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def EatsExpress():
-    return render_template("main.html", 
-                           title="EatsExpress")
+def home():
+    return render_template('main.html',
+                           title = "EatsExpress")
+
+@app.route('/login')
+def login():
+    return render_template('login.html',
+                           title = "Login - EatsExpress")
+
+@app.route('/register')
+def register():
+    return render_template('register.html',
+                           title = "Register -EatsExpress")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
-
