5379c48185172a564dba8f25d41ee19ae966cde2
rodynaamrfathy <rodynaamr@icloud.com>
2024-06-23 03:53:44 +0300

    ‘V2’

--- web_flask/main.py
+++ web_flask/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def EatsExpress():
-    return render_template("main.html", title="EatsExpress")
+    return render_template("/Users/rodynaamr/Desktop/EatsExpress/web_static/main.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
