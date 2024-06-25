544ab7c031b0ed2cb5220db1518b43c6a1d83c4d
Mohamed Yasser Ibrahim <138722509+moyasser20@users.noreply.github.com>
2024-06-25 11:50:08 +0000

    V2-Yassora

--- models/Cart.py
+++ models/Cart.py
@@ -2,33 +2,31 @@ import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, Float, Table,Integer,ForeignKey
-from sqlalchemy.orm import relationship 
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
-class Cart(Base):
+class Cart(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'cart'
         user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
         item_id = Column(Integer, ForeignKey('menu_items.id'), nullable=False)
         quantity = Column(Integer, default=1)
-
     else:
         user_id = ""
         item_id = ""
         quantity = 0
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        if models.storage_t != 'db':
-        
-            @property
-            def products(self):
-                """getter attribute returns the list of Product instances"""
-                from models.Cart import Cart
-                product_list = []
-                all_products = models.storage.all(Cart)
-                for product in all_products.values():
-                    if product.order_id == self.id:
-                        product_list.append(Cart)
-                return product_list
+    if models.storage_t != 'db':
+        @property
+        def products(self):
+            """getter attribute returns the list of Product instances"""
+            from models.Cart import Cart
+            product_list = []
+            all_products = models.storage.all(Cart)
+            for product in all_products.values():
+                if product.order_id == self.id:
+                    product_list.append(product)
+            return product_list
--- models/MenuItem.py
+++ models/MenuItem.py
@@ -1,35 +1,29 @@
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, Float, Table,Integer,ForeignKey
-from sqlalchemy.orm import relationship 
+from sqlalchemy import Column, String, Integer, ForeignKey
 
-class MenuItem(Base):
+class MenuItem(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'menu_items'
         name = Column(String(128), nullable=False)
         price = Column(Integer, nullable=False)
         restaurant_id = Column(Integer, ForeignKey('restaurants.id'), nullable=False)
-
     else:
         name = ""
         price = 0
         restaurant_id = ""
 
     def __init__(self, *args, **kwargs):
-         super().__init__(*args, **kwargs)
-
+        super().__init__(*args, **kwargs)
 
     if models.storage_t != 'db':
-        
-                @property
-                def products(self):
-                    """getter attribute returns the list of Product instances"""
-                    from models.MenuItem import MenuItem
-                    product_list = []
-                    all_products = models.storage.all(MenuItem)
-                    for product in all_products.values():
-                        if product.order_id == self.id:
-                            product_list.append(MenuItem)
-                    return product_list
+        @property
+        def products(self):
+            """Getter attribute returns the list of Product instances"""
+            from models.MenuItem import MenuItem
+            product_list = []
+            all_products = models.storage.all(MenuItem)
+            for product in all_products.values():
+                if product.order_id == self.id:
+                    product_list.append(product)
+            return product_list
