8d06f889ddbbaf953a614de89c4f99e0a3a40c8b
rodynaamrfathy <rodynaamr@icloud.com>
2024-06-27 23:03:58 +0300

    @rodynaamrfathy

--- web_flask/main.py
+++ web_flask/main.py
@@ -5,11 +5,14 @@ from models.User import User
 from models.Cart import Cart
 from models.Restaurant import Restaurant
 from models.MenuItem import MenuItem
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
+
 @app.route('/')
 def main():
     return render_template('main.html', title="EatsExpress - Home")
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -29,6 +32,7 @@ def login():
             flash('Invalid credentials, please try again.', 'danger')
             return redirect(url_for('login'))
     return render_template('login.html', title="EatsExpress - Login")
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -48,9 +52,11 @@ def register():
         flash('Account created successfully!', 'success')
         return redirect(url_for('login'))
     return render_template('create_account.html', title="EatsExpress - Register")
+
 @app.route('/add_to_cart')
 def add_to_cart():
     return render_template("viewcart.html")
+
 @app.route('/restaurant/<int:restaurant_id>')
 def restaurant(restaurant_id):
     restaurant = storage.get(Restaurant, restaurant_id)
@@ -59,18 +65,23 @@ def restaurant(restaurant_id):
         return redirect(url_for('home'))
     menu_items = [item for item in storage.all(MenuItem).values() if item.restaurant_id == restaurant_id]
     return render_template('restaurant.html', restaurant=restaurant, menu_items=menu_items, title="EatsExpress - Restaurant")
+
 @app.route('/home')
 def home():
     return render_template('main_loggedin.html', title="EatsExpress - Home")
+
 @app.route('/search')
 def search():
     return render_template('filter.html', title="EatsExpress - Search")
+
 @app.route('/item')
 def item():
     return render_template('add_item_to_cart.html', title="EatsExpress - item")
+
 @app.route('/addaddress')
 def addaddress():
     return render_template('add_address.html', title="EatsExpress - add address")
+
 @app.route('/completeorder')
 def completeorder():
     return render_template('complete_order.html', title="EatsExpress - complete order")
