d12009db9400e8b6e2ba754fb48291876c78a9ba
Mohamed Yasser Ibrahim <138722509+moyasser20@users.noreply.github.com>
2024-06-29 09:18:27 +0000

    v2

--- web_flask/models/Restaurant.py
+++ web_flask/models/Restaurant.py
@@ -3,16 +3,19 @@
 
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, Float, Table
 from sqlalchemy.orm import relationship
+
 
 class Restaurant(BaseModel, Base):
     """ Representation of a Restaurant """
-    if models.storage_t == 'db':
+    if models.storage_t =='db':
         __tablename__ = 'restaurants'
         name = Column(String(128), nullable=False)
         location = Column(String(256), nullable=True)
-        menu_items = relationship("MenuItem", backref="restaurant")  # One-to-many relationship with MenuItem
+        reviews = relationship("Review", backref="restaurant")
 
     else:
         name = ""
@@ -21,17 +24,3 @@ class Restaurant(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """ Initializes the restaurant """
         super().__init__(*args, **kwargs)
-
-        
-    def to_dict(self):
-
-        """Returns a dictionary representation of the restaurant"""
-        return {
-            'id': self.id,
-            'name': self.name,
-            'location': self.location,
-            'image_filename': self.image_filename,
-            'delivery_fee': self.delivery_fee,
-            'delivery_time': self.delivery_time,
-            'tags': [{'name': tag.name} for tag in self.tags]
-        }
