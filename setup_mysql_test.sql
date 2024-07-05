9900d2d45a90f6b85c5e136babaf2687a2b7f7f7
rodynaamrfathy <rodynaamr@icloud.com>
2024-07-05 19:48:37 +0300

    deleted useless files

--- setup_mysql_test.sql
+++
@@ -1,7 +0,0 @@
--- prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS EatExpress_test_db;
-CREATE USER IF NOT EXISTS 'EatExpress_test'@'localhost' IDENTIFIED BY 'EatExpress_test_pwd';
-GRANT ALL PRIVILEGES ON `EatExpress_test_db`.* TO 'EatExpress_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'EatExpress_test'@'localhost';
-FLUSH PRIVILEGES;
