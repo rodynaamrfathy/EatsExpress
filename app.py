4974508db487afee2b191d8b3fcd2d80368b85d4
rodynaamrfathy <rodynaamr@icloud.com>
2024-07-09 14:47:40 +0300

    Added comments to templates and added errorhandler for 404 and 500 errors

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
@@ -12,6 +13,16 @@ from routes.admin import *
 from routes.cart import *
 from routes.orders import *
 from routes.restaurants import *
+
+# Error handling for 404 - Page Not Found
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+# Error handling for 500 - Internal Server Error
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
